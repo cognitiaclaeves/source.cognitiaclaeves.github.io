325bf8cd79e7415940db6b9ffc6bb314aad94724
Cognitiaclaeves <cognitiaclaeves@gmail.com>
2016-03-06 23:54:48 -0600

    Added a few source files used to run Jekyll as a vagrant container.

---
+++ source-files/exec-jekyll.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cd /home/vagrant/data
+docker stop jekyll_runtime 2> /dev/null
+docker rm -v jekyll_runtime 2> /dev/null
+
+drafts=''
+
+if [ -f '/vagrant/drafts' ]; then
+  drafts='--drafts'
+fi
+
+
+docker run \
+	--env FORCE_POLLING=true \
+	--env JEKYLL_ENV=development \
+	--env VERBOSE=true \
+	--label=jekyll \
+	--name=jekyll_runtime \
+	--publish "0.0.0.0:4000:80" \
+	--rm \
+	--volume="$(pwd):/srv/jekyll" \
+	jekyll/jekyll:pages jekyll build --watch "$drafts"
+
