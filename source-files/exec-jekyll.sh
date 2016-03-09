f3305cea043ec71078ba3e2a0ef6d72f4f1b824d
Cognitiaclaeves <cognitiaclaeves@gmail.com>
2016-03-08 22:36:08 -0600

    Updated source files for shortcut to environment.

--- source-files/exec-jekyll.sh
+++ source-files/exec-jekyll.sh
@@ -6,11 +6,26 @@ docker rm -v jekyll_runtime 2> /dev/null
 
 drafts=''
 
+
+if [ ! -f /home/vagrant/data/_config.yml ];then
+	echo "Initializing Jekyll Environment!"
+
+	cd /home/vagrant/data
+	docker run \
+		--label=jekyll \
+		--publish 4000:4000 \
+		--rm \
+		--volume="$(pwd):/srv/jekyll" \
+		jekyll/jekyll:pages jekyll new . --force
+fi
+
 if [ -f '/vagrant/drafts' ]; then
   drafts='--drafts'
 fi
 
+echo "Running Jekyll!"
 
+cd /home/vagrant/data
 docker run \
 	--env FORCE_POLLING=true \
 	--env JEKYLL_ENV=development \
